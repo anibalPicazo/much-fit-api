713595aca22bcd5144428e18661945c85ef6e457
Anibal <anibal.picazo@hotmail.com>
2019-10-01 12:48:18 +0200

    prepare refactor

--- src/Entity/DiaDieta.php
+++ src/Entity/DiaDieta.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\UuidTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -11,12 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class DiaDieta
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use UuidTrait;
 
     /**
      * @ORM\Column(type="string", length=255)
